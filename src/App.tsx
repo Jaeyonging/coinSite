c9a8e28b8b72783914d9ec7086da396a1ee2318c
jaeyonging <wodyd1318@naver.com>
2024-06-17 17:48:21 +0900

    add: ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { Suspense, useState } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Suspense, useEffect, useState } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/Header";
 import { Home } from "./routes/Home";
 import { Socket } from "./routes/Socket";
 import { Footer } from "./components/Footer";
+import ReactGA from 'react-ga4';
+
+const TRACKING_ID = import.meta.env.VITE_GA_PROPERTYID;
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.send('pageview');
+  }, [location]);
   return (
     <>
       <Header></Header>
