5d1efc97a4178671bd642c970e50ad25cd33b033
jaeyonging <wodyd1318@naver.com>
2024-07-22 10:03:49 +0900

    test

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,12 +3,6 @@ import { Coin } from "../types/coin";
 
 const BASE_URL = 'https://api.upbit.com/v1';
 
-export async function getMarketList(): Promise<Coin> {
-    const query = '/market/all?isDetails=false';
-    const response = await axios.get(BASE_URL + query);
-    return response.data;
-}
-
 export async function FetchTodayDollar() {
     return axios.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vQF1KDUxPDqapqgXa3qyOoyWQ7ndB8hvi4Ct0FKxGXW0wofPOLbLyqWuQeGRkvvEjhwTuwmmQ7hyM9m/pub?gid=0&single=true&output=csv")
         .then((response) => {
@@ -20,19 +14,30 @@ export async function FetchTodayDollar() {
 }
 
 export async function FetchKrwCoins() {
-    const response = await axios.get("/api/v1/market/all?isDetails=false"
-    )
-    return response.data
+    const response = await axios.get("/api/v1/market/all?isDetails=false", {
+        transformRequest: [(data, headers) => {
+            delete headers['Origin'];
+            return data;
+        }]
+    });
+    return response.data;
 }
+
 
 export async function FetchKrwPrice(markets: string) {
     if (markets.length < 1) {
-        return
+        return;
     }
-    const response = await axios.get(`/api/v1/ticker?markets=${markets}`
-    )
-    return response.data
+    const response = await axios.get(`/api/v1/ticker?markets=${markets}`, {
+        transformRequest: [(data, headers) => {
+            delete headers['Origin'];
+            return data;
+        }]
+    });
+    return response.data;
 }
+
+
 
 
 export async function FetchDollarPrice() {
